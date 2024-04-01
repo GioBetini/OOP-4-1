524c30b56807f79ea9bf4297b6aaafb69227c4d0
Giovani Betini <82678579+GioBetini@users.noreply.github.com>
2024-04-01 14:45:57 -0300

    Update Trapezio.java

--- Lote4 EX1/src/Model/Trapezio.java
+++ Lote4 EX1/src/Model/Trapezio.java
@@ -15,7 +15,7 @@ public class Trapezio extends Quadrilatero{
 
     @Override
     public float getPerimetro() {
-        return getAltura() * getBase(); /*eu sei que não é isso, eu devia usar a 
+        return 2 * getAltura() + getBase() + baseMenor; /*eu sei que não é isso, eu devia usar a 
         diferença entre as bases para calcular um dos catetos de um triângulo retângulo,
         usar a altura como outro cateto e depois disso descobrir a lateral do trapézio
         que seria a hipotenusa desse triângulo, fazendo então a hipotenusa vezes 2 e
